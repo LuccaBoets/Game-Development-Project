bedd1b71f40b7baebbef0e34d98ec04ce8d6f262
Lucca Boets <boetslucca@gmail.com>
2021-12-10 17:46:41 +0100

    Create Component.cs

---
+++ Game-Development-Project/GameEngine/Component.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine
+{
+    public abstract class Component
+    {
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBacth);
+
+        public abstract void Update(GameTime gameTime);
+
+    }
+}
