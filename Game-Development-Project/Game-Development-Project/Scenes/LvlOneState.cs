966748b64792ff7db3c36265575a78c4de226b9d
Lucca Boets <boetslucca@gmail.com>
2021-12-12 18:49:27 +0100

    Fix

--- Game-Development-Project/Game-Development-Project/Scenes/LvlOneState.cs
+++ Game-Development-Project/Game-Development-Project/Scenes/LvlOneState.cs
@@ -109,8 +109,8 @@ namespace GameEngine.Scenes
             //tilemap.addTiles(MainGame.Content.Load<Texture2D>("lvl1.-2"), MainGame.GraphicsDevice, -2);
 
 
-            TileFactory.load(MainGame.GraphicsDevice, MainGame.Content.Load<Texture2D>("../../../Content/ExportedTileSet"));
-            using (FileStream fs = File.OpenRead(@"ExportedTilemapData.txt"))
+            TileFactory.load(MainGame.GraphicsDevice, MainGame.Content.Load<Texture2D>("ExportedTileSet"));
+            using (FileStream fs = File.OpenRead(@"../../../Content/ExportedTilemapData.txt"))
             {
                 tilemap = new Tilemap(fs);
             }
